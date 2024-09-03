74eb673210318b782fc4e541c57963b79c4f2950
Riley Mathews <rileymathews80@gmail.com>
2024-09-03 04:51:36 -0500

    remove hard coded enrollment secret

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -4,4 +4,15 @@
     slack
     zoom-us
   ];
+  sops.secrets."kolide_enrollment_secret" = {};
+  sops.secrets."work_internal_ssl_certificate" = {};
+
+  services.kolide-launcher.enable = true;
+  environment.etc."kolide-k2/secret" = {
+    mode = "0600";
+    source = config.sops.secrets."kolide_enrollment_secret".path;
+  };
+  
+  # TODO: figure out a way to get this working through sops or other secrets method?
+  security.pki.certificateFiles = [/home/riley/.secrets/work_ssl_cert];
 }
