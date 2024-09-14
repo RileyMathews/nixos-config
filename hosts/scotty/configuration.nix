bcd2b85be646491d1e824af277fe9f7826cebd50
Riley Mathews <rileymathews80@gmail.com>
2024-09-14 09:00:49 -0500

    moved more things into base

--- hosts/scotty/configuration.nix
+++ hosts/scotty/configuration.nix
@@ -19,7 +19,6 @@
       # ../../modules/postgres
     ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -28,55 +27,12 @@
   networking.hostName = "scotty"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile = "/home/riley/.config/sops/age/keys.txt";
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.riley = {
-    isNormalUser = true;
-    description = "Riley Mathews";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    packages = with pkgs; [];
-    shell = pkgs.zsh;
-  };
-
-  programs.firefox.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  programs.zsh.enable = true;
-
-  services.tailscale.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -85,5 +41,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
--- modules/base/default.nix
+++ modules/base/default.nix
@@ -2,6 +2,39 @@
 {
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true;
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+    sops.defaultSopsFile = ../../secrets/secrets.yaml;
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.riley = {
+        isNormalUser = true;
+        description = "Riley Mathews";
+        extraGroups = [ "networkmanager" "wheel" "docker" ];
+        packages = with pkgs; [];
+        shell = pkgs.zsh;
+    };
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
+    sops.defaultSopsFormat = "yaml";
+    sops.age.keyFile = "/home/riley/.config/sops/age/keys.txt";
 
     virtualisation.docker = {
         enable = true;
@@ -12,4 +45,12 @@
         nssmdns4 = true;
         openFirewall = true;
     };
+
+    programs.firefox.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+
+    programs.zsh.enable = true;
+
+    services.tailscale.enable = true;
 }
