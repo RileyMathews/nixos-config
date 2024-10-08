1d7a75657a8ac55774e8f47d7d2076c31eec1d1b
Riley Mathews <rileymathews80@gmail.com>
2024-10-08 17:01:52 -0500

    added rabbitmq vm

---
+++ hosts/rabbitmq-vm/configuration.nix
@@ -0,0 +1,28 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./../../modules/vms/basic-disk-config.nix
+    ./../../modules/vms/basic-hardware-config.nix
+    ./../../modules/vms/basic-config.nix
+  ];
+
+  users.users.riley = {
+    isNormalUser = true;
+    description = "Riley Mathews";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgBrMhlYyFQuzLE2dEIJ/vEEN769EiPrpKYVzBKERoe rileymathews80@gmail.com"];
+  };
+
+  programs.zsh.enable = true;
+
+  services.tailscale.enable = true;
+
+  networking.hostName = "rabbitmq";
+}
