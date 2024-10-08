e1f563f78d2279cfe6b04bb3370e889be9b53eaa
Riley Mathews <rileymathews80@gmail.com>
2024-10-08 10:50:42 -0500

    redis config

--- hosts/redis-vm/configuration.nix
+++ hosts/redis-vm/configuration.nix
@@ -10,6 +10,10 @@
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
   ];
+  services.redis.servers."".enable = true;
+  services.redis.servers."".bind = "0.0.0.0";
+  services.redis.servers."".openFirewall = true;
+  services.redis.servers."".extraParams = ["--protected-mode no"];
 
   networking.hostName = "redis";
 }
