ba6b6d836f5c97472e29290a54800f4ec48bf778
Riley Mathews <rileymathews80@gmail.com>
2024-10-13 16:33:25 -0500

    proxy straight from voyager to rpgweave

--- hosts/rpgweave-production-vm/configuration.nix
+++ hosts/rpgweave-production-vm/configuration.nix
@@ -9,7 +9,6 @@
     ./../../modules/vms/basic-disk-config.nix
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
-    ./../../modules/nginx-single-proxy
   ];
 
   virtualisation.docker = {
@@ -17,25 +16,7 @@
     enableOnBoot = true;
   };
 
-  myNginx = {
-    enable = true;
-    hostName = "rpgweave.com";
-    reverseProxyAddress = "http://127.0.0.1:8000";
-  };
-
-  services.nginx.virtualHosts."rpgweave.com".listen = [
-    {
-      addr = "0.0.0.0";
-      port = 80;
-      proxyProtocol = true;
-    }
-    {
-      addr = "0.0.0.0";
-      port = 443;
-      ssl = true;
-      proxyProtocol = true;
-    }
-  ];
+  networking.firewall.allowedTCPPorts = [8000];
 
   networking.hostName = "rpgweave-production";
 }
