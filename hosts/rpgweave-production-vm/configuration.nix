ce907c4a5baaad34648b71afb02d1b4fa7b0ecb4
Riley Mathews <rileymathews80@gmail.com>
2024-10-13 16:12:11 -0500

    fixed rpgweave vms

--- hosts/rpgweave-production-vm/configuration.nix
+++ hosts/rpgweave-production-vm/configuration.nix
@@ -9,12 +9,33 @@
     ./../../modules/vms/basic-disk-config.nix
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
+    ./../../modules/nginx-single-proxy
   ];
 
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
   };
+
+  myNginx = {
+    enable = true;
+    hostName = "rpgweave.com";
+    reverseProxyAddress = "http://127.0.0.1:8000";
+  };
+
+  services.nginx.virtualHosts."rpgweave.com".listen = [
+    {
+      addr = "0.0.0.0";
+      port = 80;
+      proxyProtocol = true;
+    }
+    {
+      addr = "0.0.0.0";
+      port = 443;
+      ssl = true;
+      proxyProtocol = true;
+    }
+  ];
 
   networking.hostName = "rpgweave-production";
 }
