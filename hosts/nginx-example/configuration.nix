3a3b75049081357d9c73bd3c6a91f52d09399eb5
Riley Mathews <rileymathews80@gmail.com>
2025-09-10 20:17:40 -0400

    use different hostname

--- hosts/nginx-example/configuration.nix
+++ hosts/nginx-example/configuration.nix
@@ -19,7 +19,7 @@
 
   myNginx = {
     enable = true;
-    hostName = "nginx-example.rileymathews.com";
+    hostName = "nixos-example.rileymathews.com";
     reverseProxyAddress = "http://127.0.0.1:8000";
   };
 }
