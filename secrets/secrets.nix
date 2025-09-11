5480d404998db7b1e870344482480f6ba8ea60e4
Riley Mathews <rileymathews80@gmail.com>
2025-09-11 13:36:31 -0400

    edited secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,8 +2,8 @@ let
   # Your user SSH public key
   riley = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgBrMhlYyFQuzLE2dEIJ/vEEN769EiPrpKYVzBKERoe rileymathews80@gmail.com";
   nixos-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0BPGd3sFaR0lrLuG6STXWqJHowivuXf2s02TSE/eQj root@nixos-test";
-  borg = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTZ+ZwdQ/Eh+qx8PLe5kPk6SYO0+Nn+V7n5WyJNO+4J root@nixos"
-  pg17 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvOTkyp4FxnjriR+Y3E25kaBKdtXWekYph3m9SWSzLX root@nixos"
+  borg = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTZ+ZwdQ/Eh+qx8PLe5kPk6SYO0+Nn+V7n5WyJNO+4J root@nixos";
+  pg17 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvOTkyp4FxnjriR+Y3E25kaBKdtXWekYph3m9SWSzLX root@nixos";
   all = [riley nixos-test borg pg17];
 in
 {
