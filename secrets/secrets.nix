428ec2911a038c066d2065feb56f8fd5af2fd3c9
Riley Mathews <rileymathews80@gmail.com>
2025-09-11 14:12:12 -0400

    reset keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,7 @@ let
   nixos-test = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0BPGd3sFaR0lrLuG6STXWqJHowivuXf2s02TSE/eQj root@nixos-test";
   borg = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTZ+ZwdQ/Eh+qx8PLe5kPk6SYO0+Nn+V7n5WyJNO+4J root@nixos";
   pg17 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvOTkyp4FxnjriR+Y3E25kaBKdtXWekYph3m9SWSzLX root@nixos";
-  playground = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVaKFUEABrVlZPcJqx+nanZ4R/1r46AT4fMqbEnry4v root@nixos";
+  playground = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEdaKF4qDveUOE7EDKQl5hwUfTh1lPkfpuhnAZVRZM58 root@nixos-playground";
   all = [riley nixos-test borg pg17 playground];
 in
 {
