e05b3814f9179183df5bf94bceaa11539c69a8ec
Riley Mathews <rileymathews80@gmail.com>
2025-09-11 13:55:28 -0400

    fixed pg

--- hosts/postgres-17/configuration.nix
+++ hosts/postgres-17/configuration.nix
@@ -9,7 +9,7 @@
     ./../../modules/vms/basic-disk-config.nix
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
-    ./../../modules/tailscale/default.nix
+    ./../../modules/tailscale
   ];
   networking.hostName = "pg17";
 
