dd98656fd482bb5235fc0252a19c7fa333bb7241
Riley Mathews <rileymathews80@gmail.com>
2025-09-11 15:52:33 -0400

    forgejo bootstraped

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,12 +5,13 @@ let
   borg = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTZ+ZwdQ/Eh+qx8PLe5kPk6SYO0+Nn+V7n5WyJNO+4J root@nixos";
   pg17 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvOTkyp4FxnjriR+Y3E25kaBKdtXWekYph3m9SWSzLX root@nixos";
   playground = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEdaKF4qDveUOE7EDKQl5hwUfTh1lPkfpuhnAZVRZM58 root@nixos-playground";
-  all = [riley nixos-test borg pg17 playground];
+  forgejo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKaNVYYQM1GrTxmROiI82gPlhYO3WOddhak9ks6NhvRu root@forgejo";
+  all = [riley nixos-test borg pg17 playground forgejo];
 in
 {
   # Cloudflare credentials for ACME DNS challenge
   "cloudflare-credentials.age".publicKeys = all;
   "tailscale-credentials.age".publicKeys = all;
   "cloudflare-api-key.age".publicKeys = all;
-  "forgejo-database-password.age".publicKeys = all;
+  "forgejo-database-password.age".publicKeys = [forgejo];
 }
