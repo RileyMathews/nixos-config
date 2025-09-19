99a445d5adb38b7fecf45c9e75323a70cb788f4b
Riley Mathews <rileymathews80@gmail.com>
2025-09-19 16:52:40 -0400

    fix searxng

--- modules/searxng/default.nix
+++ modules/searxng/default.nix
@@ -2,6 +2,7 @@
     config,
     modulesPath,
     lib,
+    unstablePkgs,
     ...
 }:
 {
@@ -9,11 +10,15 @@
     services.cloudflare-dns.enable = true;
     services.cloudflare-dns.domains = ["asearch.rileymathews.com"];
 
-    myNginx.proxies.searxng = {
+    myNginx.proxies.asearxng = {
         listenHost = "asearch.rileymathews.com";
         backendHost = "http://127.0.0.1:8050";
     };
 
+    services.searx.package = unstablePkgs.searxng;
     services.searx.enable = true;
-    services.searx.settings.server.port = 8050;
+    services.searx.settings = {
+        server.port = 8050;
+        server.secret_key = "super-secret-key";
+    };
 }
