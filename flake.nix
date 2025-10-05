5f33ca51b5d1c2c92a38e6ca94fce4d3ae4f7e50
Riley Mathews <rileymathews80@gmail.com>
2025-10-05 18:40:39 -0400

    updates

--- flake.nix
+++ flake.nix
@@ -59,6 +59,7 @@
 
           playground = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
+            specialArgs = {inherit system unstablePkgs; };
             modules = [
               disko.nixosModules.disko
               agenix.nixosModules.default
--- hosts/playground/configuration.nix
+++ hosts/playground/configuration.nix
@@ -11,57 +11,9 @@
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
     ./../../modules/tailscale
-    ./../../modules/dns
-    ./../../modules/nginx-multi-proxy
   ];
   networking.hostName = "nixos-playground";
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
-
-  services.rpcbind.enable = true;
-  boot.kernelModules = [ "nfs" ];
-  boot.supportedFilesystems = [ "nfs" ];
-  fileSystems."/mnt/testing" = {
-    device = "nas:/main/testing";
-    fsType = "nfs";
-    options = ["defaults"];
-  };
-  services.cloudflare-dns = {
-    enable = true;
-    domains = [
-      "testing.rileymathews.com"
-      "whoaminixos.rileymathews.com"
-    ];
-  };
-
-  myNginx.proxies.whoami1 = {
-    listenHost = "testing.rileymathews.com";
-    backendHost = "http://127.0.0.1:8000";
-  };
-  myNginx.proxies.whoami2 = {
-    listenHost = "whoaminixos.rileymathews.com";
-    backendHost = "http://127.0.0.1:8001";
-  };
-
-  virtualisation.podman.enable = true;
-
-  systemd.timers."podman-auto-update".wantedBy = ["multi-user.target"];
-
-  virtualisation.oci-containers.containers = {
-    whoami = {
-      image = "docker.io/traefik/whoami:latest";
-      ports = ["8000:80"];
-      extraOptions = [
-        "--label" "io.containers.autoupdate=registry"
-      ];
-    };
-    whoami2 = {
-      image = "docker.io/traefik/whoami:v1.11";
-      ports = ["8001:80"];
-      extraOptions = [
-        "--label" "io.containers.autoupdate=registry"
-      ];
-    };
-  };
 }
 
