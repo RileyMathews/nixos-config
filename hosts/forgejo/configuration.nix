8e5f8dfd1145b3a72ef5f813375e69f9e0465523
Riley Mathews <rileymathews80@gmail.com>
2025-10-20 13:33:51 -0400

    update flakes

--- hosts/forgejo/configuration.nix
+++ hosts/forgejo/configuration.nix
@@ -79,6 +79,7 @@
     user = "git";
     group = "git";
     stateDir = "/mnt/forgejo";
+    package = unstablePkgs.forgejo;
     settings = {
       server = {
         ROOT_URL = "https://git.rileymathews.com";
