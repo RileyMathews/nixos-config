ccaa16693b0eb04af4caa8fa8d4b1c903facacff
Riley Mathews <dev@rileymathews.com>
2025-12-19 11:16:40 -0600

    start moving karakeep to docker

--- modules/karakeep/default.nix
+++ modules/karakeep/default.nix
@@ -14,14 +14,15 @@
         backendHost = "http://127.0.0.1:3000";
     };
 
-    services.karakeep.enable = true;
-    age.secrets.karakeep-env = {
-        file = ../../secrets/karakeep-env.age;
-        mode = "0400";
-        owner = "karakeep";
-        group = "karakeep";
-    };
-    services.karakeep.environmentFile = config.age.secrets.karakeep-env.path;
-    systemd.services."karakeep".after = [ "network.target" "run-agenix.d.mount" ];
-    systemd.services."karakeep".requires = [ "run-agenix.d.mount" ];
+    # services.karakeep.enable = true;
+    # age.secrets.karakeep-env = {
+    #     file = ../../secrets/karakeep-env.age;
+    #     mode = "0400";
+    #     owner = "karakeep";
+    #     group = "karakeep";
+    # };
+    # services.karakeep.package = unstablePkgs.karakeep;
+    # services.karakeep.environmentFile = config.age.secrets.karakeep-env.path;
+    # systemd.services."karakeep".after = [ "network.target" "run-agenix.d.mount" ];
+    # systemd.services."karakeep".requires = [ "run-agenix.d.mount" ];
 }
