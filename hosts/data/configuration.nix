6271a4347b83b2705020fdffc99ab233ced06abb
Riley Mathews <dev@rileymathews.com>
2025-12-24 23:10:26 -0600

    added nvidia to data vm

--- hosts/data/configuration.nix
+++ hosts/data/configuration.nix
@@ -17,6 +17,12 @@
   networking.hostName = "data";
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
+  hardware.nvidia-container-toolkit.enable = true;
+  # hardware.nvidia.datacenter.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
 
   virtualisation.podman.enable = true;
 }
