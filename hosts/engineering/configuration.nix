9750e99521a9dd168cd0fdd0e294c6a0b3dde5fd
Riley Mathews <dev@rileymathews.com>
2026-01-12 23:53:41 -0600

    added grafana and prometheus

--- hosts/engineering/configuration.nix
+++ hosts/engineering/configuration.nix
@@ -12,8 +12,33 @@
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
     ./../../modules/tailscale
+    ./../../modules/dns
+    ./../../modules/nginx-multi-proxy
   ];
   networking.hostName = "engineering";
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
+  services.cloudflare-dns.enable = true;
+  services.cloudflare-dns.domains = [ "grafana.rileymathews.com" ];
+
+  myNginx.proxies.grafana = {
+    listenHost = "grafana.rileymathews.com";
+    backendHost = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
+  };
+
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        domain = "grafana.rileymathews.com";
+        http_port = 2342;
+        http_addr = "127.0.0.1";
+      };
+    };
+  };
+
+  services.prometheus = {
+    enable = true;
+    port = 9001;
+  };
 }
