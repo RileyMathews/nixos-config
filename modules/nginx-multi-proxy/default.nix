fca65c6498a8d1aaec634d0324d6298672a22a9a
Riley Mathews <rileymathews80@gmail.com>
2026-01-25 20:20:43 -0600

    better nginx

--- modules/nginx-multi-proxy/default.nix
+++ modules/nginx-multi-proxy/default.nix
@@ -49,7 +49,12 @@
                     locations."/" = {
                         proxyPass = proxyConfig.backendHost;
                         proxyWebsockets = true;
-                        extraConfig = "proxy_set_header            Upgrade $http_upgrade;\nproxy_set_header            Connection \"Upgrade\";\nproxy_set_header            X-Real-IP $remote_addr;\nproxy_set_header            X-Forward-For $proxy_add_x_forwarded_for;";
+                        extraConfig = ''
+                            proxy_set_header Upgrade $http_upgrade;
+                            proxy_set_header Connection "Upgrade";
+                            proxy_set_header X-Real-IP $remote_addr;
+                            proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+                        '';
                     };
                     listen = [
                         { addr = "0.0.0.0"; port = 80; proxyProtocol = proxyConfig.proxyProtocol; }
