66769128198d50fe921ec1c626c2d0ecca6e103d
Riley Mathews <rileymathews80@gmail.com>
2026-02-12 08:14:03 -0600

    migrate mealie

--- modules/mealie/default.nix
+++ modules/mealie/default.nix
@@ -1,9 +1,9 @@
-{ config, lib, ... }:
+{ config, ... }:
 {
     imports = [
-        ../nas-oci
         ../nginx-multi-proxy
         ../dns
+        ../restic-local-appdata
     ];
 
     services.cloudflare-dns.enable = true;
@@ -18,37 +18,35 @@
         file = ../../secrets/mealie-credentials-file.age;
     };
 
-    services.nasOci = {
+    services.resticLocalAppdata = {
         enable = true;
+        paths = [
+            "/var/lib/appdata/mealie/app/data"
+        ];
+    };
 
-        mounts.mealie = {
-            mountPoint = "/mnt/mealie";
-            device = "nas:/mealie";
-        };
-
-        containers.mealie = {
-            definition = {
-                image = "ghcr.io/mealie-recipes/mealie:v3.10.2";
-                ports = [ "9000:9000" ];
-                volumes = [ "/mnt/mealie/app/data:/app/data" ];
-                user = "1000:1000";
-                environment = {
-                    ALLOW_SIGNUP = "true";
-                    PUID = "1000";
-                    GUID = "1000";
-                    TZ = "America/Chicago";
-                    DB_ENGINE = "postgres";
-                    POSTGRES_USER = "mealie";
-                    POSTGRES_SERVER = "pg17.tailscale.rileymathews.com";
-                    POSTGRES_PORT = "5432";
-                    POSTGRES_DB = "mealie";
-                    MAX_WORKERS = "1";
-                    WEB_CONCURRENCY = "1";
-                    BASE_URL = "mealie.rileymathews.com";
-                    TOKEN_TIME = "720";
-                };
-                environmentFiles = [ config.age.secrets.mealie-credentials-file.path ];
+    virtualisation.oci-containers.containers = {
+        mealie = {
+            image = "ghcr.io/mealie-recipes/mealie:v3.10.2";
+            ports = [ "9000:9000" ];
+            volumes = [ "/var/lib/appdata/mealie/app/data:/app/data" ];
+            user = "1000:1000";
+            environment = {
+                ALLOW_SIGNUP = "true";
+                PUID = "1000";
+                GUID = "1000";
+                TZ = "America/Chicago";
+                DB_ENGINE = "postgres";
+                POSTGRES_USER = "mealie";
+                POSTGRES_SERVER = "pg17.tailscale.rileymathews.com";
+                POSTGRES_PORT = "5432";
+                POSTGRES_DB = "mealie";
+                MAX_WORKERS = "1";
+                WEB_CONCURRENCY = "1";
+                BASE_URL = "mealie.rileymathews.com";
+                TOKEN_TIME = "720";
             };
+            environmentFiles = [ config.age.secrets.mealie-credentials-file.path ];
         };
     };
 }
