b5e043e8326d7b6de281d2712d5a84419986fbf0
Riley Mathews <rileymathews80@gmail.com>
2026-02-12 12:49:33 -0600

    migrate paperless and registry

--- modules/docker-registry/default.nix
+++ modules/docker-registry/default.nix
@@ -1,12 +1,8 @@
 {
-    config,
-    modulesPath,
-    lib,
-    unstablePkgs,
     ...
 }:
 {
-    imports = [../nginx-multi-proxy ../dns];
+    imports = [../nginx-multi-proxy ../dns ../restic-local-appdata];
     services.cloudflare-dns.enable = true;
     services.cloudflare-dns.domains = ["registry.rileymathews.com"];
 
@@ -15,28 +11,11 @@
         backendHost = "http://127.0.0.1:5000";
     };
 
-    systemd.mounts = [{
-        what = "nas:/main/docker_images";
-        where = "/mnt/docker_images";
-        type = "nfs";
-        options = "defaults";
-
-        # Make it wait for Tailscale
-        wantedBy = [ "multi-user.target" ];
-        after = [ "tailscale-ready.service" ];
-        requires = [ "tailscale-ready.service" ];
-    }];
-
-    systemd.services."podman-registry".unitConfig = {
-        Requires = [ "mnt-docker_images.mount" ];
-        After = [ "mnt-docker_images.mount" ];
-    };
-
     virtualisation.oci-containers.containers = {
         registry = {
             image = "registry:3.0.0";
             ports = ["5000:5000"];
-            volumes = [ "/mnt/docker_images:/var/lib/registry" ];
+            volumes = [ "/var/lib/appdata/docker-registry:/var/lib/registry" ];
         };
     };
 }
