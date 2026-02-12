cbe6d63e4a7aa7fe41f40afbfd63ea54a878db9f
Riley Mathews <rileymathews80@gmail.com>
2026-02-12 08:25:39 -0600

    migrate vaultwarden

--- modules/vaultwarden/default.nix
+++ modules/vaultwarden/default.nix
@@ -6,7 +6,7 @@
     ...
 }:
 {
-    imports = [ ../nas-oci ];
+    imports = [ ../restic-local-appdata ];
 
     networking.firewall.allowedTCPPorts = [8222];
 
@@ -17,28 +17,26 @@
         # group = "vaultwarden";
     };
 
-    services.nasOci = {
-        enable = true;
-
-        mounts.vaultwarden = {
-            mountPoint = "/mnt/vaultwarden";
-            device = "nas:/vaultwarden";
-        };
-
-        containers.vaultwarden = {
-            definition = {
-                image = "vaultwarden/server:1.35.3";
-                ports = ["8222:8222"];
-                volumes = [ "/mnt/vaultwarden/data:/data" ];
-                user = "1000:1000";
-                environment = {
-                    DOMAIN = "https://vaultwarden.rileymathews.com";
-                    SIGNUPS_ALLOWED = "true";
-                    ROCKET_ADDRESS = "0.0.0.0";
-                    ROCKET_PORT = "8222";
-                };
-                environmentFiles = [ config.age.secrets.vaultwarden-env-file.path ];
+    virtualisation.oci-containers.containers = {
+        vaultwarden = {
+            image = "vaultwarden/server:1.35.3";
+            ports = ["8222:8222"];
+            volumes = [ "/var/lib/appdata/vaultwarden/data:/data" ];
+            user = "1000:1000";
+            environment = {
+                DOMAIN = "https://vaultwarden.rileymathews.com";
+                SIGNUPS_ALLOWED = "true";
+                ROCKET_ADDRESS = "0.0.0.0";
+                ROCKET_PORT = "8222";
             };
+            environmentFiles = [ config.age.secrets.vaultwarden-env-file.path ];
         };
+    };
+
+    services.resticLocalAppdata = {
+        enable = true;
+        paths = [
+            "/var/lib/appdata/vaultwarden/data"
+        ];
     };
 }
