4080baa62f6dca3c8b7abacfc816015db90bce5e
Riley Mathews <rileymathews80@gmail.com>
2026-02-13 19:18:47 -0600

    add swap config module to worf

--- hosts/worf/configuration.nix
+++ hosts/worf/configuration.nix
@@ -10,6 +10,7 @@
     ./../../modules/vms/basic-disk-config.nix
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
+    ./../../modules/vms/swap-config.nix
     ./../../modules/tailscale
     ./../../modules/vaultwarden
   ];
@@ -17,4 +18,3 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
 }
-
