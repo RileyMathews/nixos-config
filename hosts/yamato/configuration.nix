ad65e1292266e4428ee4d1d74122cd35de640d1a
Riley Mathews <rileymathews80@gmail.com>
2026-02-17 23:47:37 -0600

    add swap to yamato

--- hosts/yamato/configuration.nix
+++ hosts/yamato/configuration.nix
@@ -10,6 +10,7 @@
     ./../../modules/vms/basic-disk-config.nix
     ./../../modules/vms/basic-hardware-config.nix
     ./../../modules/vms/basic-config.nix
+    ./../../modules/vms/swap-config.nix
     ./../../modules/tailscale
     ./../../modules/podman-exporter
     ./../../modules/open-webui
