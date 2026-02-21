c33bc807bbe7fc995946d39d7ff5591a86a136d9
Riley Mathews <rileymathews80@gmail.com>
2026-02-20 21:50:17 -0600

    update containers

--- modules/ollama/default.nix
+++ modules/ollama/default.nix
@@ -14,7 +14,7 @@
     };
 
     virtualisation.oci-containers.containers.ollama = {
-        image = "ollama/ollama:0.16.2";
+        image = "ollama/ollama:0.16.3";
         ports = [ "11434:11434" ];
         environment = {
             OLLAMA_HOST = "0.0.0.0:11434";
