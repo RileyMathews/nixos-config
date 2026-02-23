19970c5e87957fd819e28874a0291d7e8efb74dc
Riley Mathews <rileymathews80@gmail.com>
2026-02-23 17:18:18 -0600

    add direnv plugin

--- modules/home-manager/riley/opencode.nix
+++ modules/home-manager/riley/opencode.nix
@@ -5,6 +5,7 @@
     ".config/opencode/agent".source = ./opencode/agent;
     ".config/opencode/skills".source = ./opencode/skills;
     ".config/opencode/tools".source = ./opencode/tools;
+    ".config/opencode/plugins".source = ./opencode/plugins;
     ".config/opencode/AGENT.md".source = ./opencode/AGENT.md;
   };
 }
