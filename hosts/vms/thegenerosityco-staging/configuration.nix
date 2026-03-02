2892ffd8c182a116506e5136e306a0e1698698ca
Riley Mathews <rileymathews80@gmail.com>
2026-03-02 16:36:09 -0600

    add backup

--- hosts/vms/thegenerosityco-staging/configuration.nix
+++ hosts/vms/thegenerosityco-staging/configuration.nix
@@ -11,9 +11,17 @@
     ./../../../modules/vms/basic-hardware-config.nix
     ./../../../modules/vms/basic-config.nix
     ./../../../modules/tailscale
+    ./../../../modules/restic-local-appdata
   ];
   networking.hostName = "thegenerosityco-staging";
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
   virtualisation.docker.enable = true;
+  virtualisation.docker.logDriver = "json-file";
+  services.resticLocalAppdata = {
+      enable = true;
+      paths = [
+          "/var/lib/thegenerosityco/database"
+      ];
+  };
 }
