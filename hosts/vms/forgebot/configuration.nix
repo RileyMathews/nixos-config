5c24bb76cf2ab54741a15ec6ba020d13aca19ffb
Riley Mathews <rileymathews80@gmail.com>
2026-03-09 09:12:04 -0500

    better leader hotkey

--- hosts/vms/forgebot/configuration.nix
+++ hosts/vms/forgebot/configuration.nix
@@ -13,8 +13,18 @@
     ./../../../modules/vms/basic-hardware-config.nix
     ./../../../modules/vms/basic-config.nix
     ./../../../modules/tailscale
+    ./../../../modules/dns
+    ./../../../modules/caddy-multi-proxy
     forgebot.nixosModules.forgebot
   ];
+  services.cloudflare-dns = {
+    enable = true;
+    domains = ["forgebot.rileymathews.com"];
+  };
+  myCaddy.proxies.forgebot = {
+    listenHost = "forgebot.rileymathews.com";
+    backendHost = "http://127.0.0.1:8765";
+  };
   networking.hostName = "forgebot";
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
@@ -26,6 +36,7 @@
     enable = true;
     
     forgejo.url = "https://git.rileymathews.com";
+    server.forgeBotHost = "https://forgebot.rileymathews.com";
     secretsFilePath = config.age.secrets.forgebot-credentials-file.path;
   };
 }
