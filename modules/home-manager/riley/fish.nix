adfa84622b0420ce86bccafd66bade4053d67d56
Riley Mathews <rileymathews80@gmail.com>
2026-03-18 23:29:19 -0500

    setting up fish

--- modules/home-manager/riley/fish.nix
+++ modules/home-manager/riley/fish.nix
@@ -1,4 +1,28 @@
 { ... }:
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    binds = {
+      "ctrl-y".command = "accept-autosuggestion";
+    };
+    shellAliases = {
+      gst = "git status";
+      gaa = "git add .";
+      gcmsg = "git commit -m";
+      gp = "git push";
+      gpsup = "git push --set-upstream origin $(git branch --show-current)";
+      gl = "git pull";
+      gco = "git checkout";
+      gcb = "git checkout -b";
+      gcm = "git checkout $(git_main_branch)";
+      l = "ls -al";
+      tss = "sudo tailscale switch";
+      oc = "opencode";
+
+      mpr = "python manage.py runserver";
+      mpmm = "python manage.py makemigrations";
+      mpm = "python manage.py migrate";
+      mp = "python manage.py";
+    };
+  };
 }
--- modules/home-manager/riley/ghostty.nix
+++ modules/home-manager/riley/ghostty.nix
@@ -7,9 +7,8 @@
       gtk-titlebar = false;
       window-decoration = "server";
       confirm-close-surface = false;
-      command = "nu";
+      command = "fish";
       keybind = "ctrl+o=unbind";
     };
   };
 }
-
