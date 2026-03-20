5af53289addd38babcc961559e53e39cb20ef024
Riley Mathews <rileymathews80@gmail.com>
2026-03-20 00:06:16 -0500

    add env var to fish as well

--- modules/home-manager/riley/custom.fish
+++ modules/home-manager/riley/custom.fish
@@ -2,5 +2,7 @@ function gcb -a branch_name -d "make a new branch with worktrunk and zellij"
     wt switch -c $branch_name -x "zellij -s {{repo}}.{{branch}}"
 end
 
+set -x NVIM_APPNAME "oldnvim"
+
 zoxide init fish | source
 direnv hook fish | source
