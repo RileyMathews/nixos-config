73e71a9b1af3ab6fa726a685cae9367f889d0649
Riley Mathews <riley@mercury.com>
2026-03-20 09:22:34 -0500

    scripts

--- modules/home-manager/riley/custom.fish
+++ modules/home-manager/riley/custom.fish
@@ -2,5 +2,11 @@ function gcb -a branch_name -d "make a new branch with worktrunk and zellij"
     wt switch -c $branch_name -x "zellij -s {{repo}}.{{branch}}"
 end
 
+function gw -d "run ghciwatch with additional fields for my local workflow"
+    services up
+    make ghciwatch | tee .devel-logs/output.txt
+end
+
 zoxide init fish | source
 direnv hook fish | source
+wt config shell init fish | source
