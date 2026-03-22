09aebe44ed8f08e58b3e40c56ddc09458cb0c709
Riley Mathews <rileymathews80@gmail.com>
2026-03-22 17:48:55 -0500

    add printing

--- hosts/desktops/ds9/configuration.nix
+++ hosts/desktops/ds9/configuration.nix
@@ -68,6 +68,12 @@
         "modesetting"
       ];
     };
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
 
   environment.systemPackages = with pkgs; [ nvtopPackages.nvidia ];
