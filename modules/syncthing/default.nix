132ae28c45515034448d1c2b5f9c4e073228edfb
Riley Mathews <rileymathews80@gmail.com>
2026-04-13 23:29:12 -0500

    syncthing config

--- modules/syncthing/default.nix
+++ modules/syncthing/default.nix
@@ -16,9 +16,23 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    settings.gui = {
-      address = "127.0.0.1:8384";
-      insecureSkipHostcheck = true;
+    settings = {
+      gui = {
+        address = "127.0.0.1:8384";
+        insecureSkipHostcheck = true;
+      };
+    };
+
+    devices = {
+      "ds9" = { id = "JSFGUXS-BY7JCZN-LA6XNR3-EKJOTTR-GMJITVW-S6BNX7V-2WJ7RQA-KVAJHA7"; addresses = ["tcp://ds9.tailf1cbe3.ts.net:22000"]; };
+      "pixel9a" = { id = "EZKOKUC-EQDRSUG-DVB7BFZ-VBQOCV2-WOATBFK-5REERDU-7J4ZQLI-CKEZAQT"; addresses = ["tcp://pixel-9a.tailf1cbe3.ts.net:22000"]; };
+    };
+
+    folders = {
+      "obsidian" = {
+        path = "/main/obsidian";
+        devices = ["ds9" "pixel9a"];
+      };
     };
   };
 }
