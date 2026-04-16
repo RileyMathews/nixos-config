0f99fe09d0aeca56f98150b6c7b0215c3dc0429b
Riley Mathews <rileymathews80@gmail.com>
2026-04-15 21:17:06 -0500

    dev postgres server

--- hosts/vms/postgres-dev/configuration.nix
+++ hosts/vms/postgres-dev/configuration.nix
@@ -10,8 +10,28 @@
     ./../../../modules/vms/basic-hardware-config.nix
     ./../../../modules/vms/basic-config.nix
     ./../../../modules/tailscale
+    ./../../../modules/dns
   ];
   networking.hostName = "postgres-dev";
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_18;
+    enableTCPIP = true;
+    authentication = ''
+      local all pgadmin peer
+      local all all trust
+      host all all 0.0.0.0/0 md5
+    '';
+    settings = {
+      timezone = "UTC";
+      log_timezone = "UTC";
+    };
+  };
+  networking.firewall.allowedTCPPorts = [5432];
+  services.cloudflare-dns = {
+    enable = true;
+    domains = ["pgdev.tailscale.rileymathews.com"];
+  };
 }
