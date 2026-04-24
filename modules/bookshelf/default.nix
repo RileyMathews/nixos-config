c275cbd7ced01a0eb79c1fcc9deb15623fb64bf5
Riley Mathews <rileymathews80@gmail.com>
2026-04-24 16:33:13 -0500

    bump bookshelf

--- modules/bookshelf/default.nix
+++ modules/bookshelf/default.nix
@@ -24,7 +24,7 @@
     virtualisation.oci-containers.containers = {
         bookshelf = {
             # discovery
-            image = "registry.rileymathews.com/rileymathews/bookshelf:0.0.4";
+            image = "registry.rileymathews.com/rileymathews/bookshelf:0.0.6";
             ports = ["3847:3000"];
             user = "1000:1000";
             volumes = ["/var/lib/bookshelf/data:/app/storage"];
