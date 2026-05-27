19ecb8f12fd4e902b8409ba65fada99a52b2e9c6
Riley Mathews <rileymathews80@gmail.com>
2026-05-26 23:24:05 -0500

    make relay use caddy

--- hosts/vms/relay/configuration.nix
+++ hosts/vms/relay/configuration.nix
@@ -10,7 +10,7 @@
     ./../../../modules/vms/basic-hardware-config.nix
     ./../../../modules/vms/basic-config.nix
     ./../../../modules/tailscale
-    ./../../../modules/nginx-multi-proxy
+    ./../../../modules/caddy-multi-proxy
     ./../../../modules/dns
   ];
   services.cloudflare-dns.enable = true;
@@ -19,27 +19,27 @@
   nix.settings.experimental-features = ["nix-command" "flakes"];
   myTailscale.enable = true;
 
-  myNginx.proxies.rpgweave = {
+  myCaddy.proxies.rpgweave = {
     listenHost = "rpgweave.com";
     backendHost = "http://rpgweave:80";
     proxyProtocol = true;
   };
-  myNginx.proxies.rpgweave-staging = {
+  myCaddy.proxies.rpgweave-staging = {
     listenHost = "staging.rpgweave.com";
     backendHost = "http://rpgweave-staging:80";
     proxyProtocol = true;
   };
-  myNginx.proxies.vaultwarden = {
+  myCaddy.proxies.vaultwarden = {
     listenHost = "vaultwarden.rileymathews.com";
     backendHost = "http://worf:8222";
     proxyProtocol = true;
   };
-  myNginx.proxies.thegenerosityco-staging = {
+  myCaddy.proxies.thegenerosityco-staging = {
     listenHost = "thegenerosityco-staging.rileymathews.com";
     backendHost = "http://thegenerosityco-staging:8080";
     proxyProtocol = true;
   };
-  myNginx.proxies.papyrd-demo = {
+  myCaddy.proxies.papyrd-demo = {
     listenHost = "papyrd-demo.rileymathews.com";
     backendHost = "http://discovery:3847";
     proxyProtocol = true;
